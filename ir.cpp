6031f2d58344e691d354619f0ff7d44905842403
nan <499983129@qq.com>
2019-06-04 11:03:11 +0800

    ir.cpp

--- ir.cpp
+++ ir.cpp
@@ -28,47 +28,26 @@ enum class Pins{
 };
 
 enum class RemoteButton {
-	  //% blockId="Power" block="电源"
 	  Power = 0x0,
-	  //% blockId="Up" block="上"
 	  Up = 0x1,
-	  //% blockId="Left" block="左"
 	  Left = 0x4,
-	  //% blockId="Right" block="右"
 	  Right = 0x6,
-	  //% blockId="Down" block="下"
 	  Down = 0x9,
-	   //% blockId="Light" block="光线"
 	  Light = 0x2,
-	  //% blockId="BEEP" block="喇叭"
 	  BEEP = 0x5,
-	  //% blockId="Plus" block="加"
 	  Plus = 0xc,
-	   //% blockId="Minus" block="减"
 	  Minus = 0xe,
-	   //% blockId="TLeft" block="向左"
 	  TLeft = 0x8,
-	  //% blockId="TRight" block="向右"
 	  TRight = 0xA,
-	  //% blockId="NUM0" block="数字0"
 	  NUM0 = 0xD,
-	  //% blockId="NUM1" block="数字1"
 	  NUM1 = 0x10,
-	  //% blockId="NUM2" block="数字2"
 	  NUM2 = 0x11,
-	  //% blockId="NUM3" block="数字3"
 	  NUM3 = 0x12,
-	  //% blockId="NUM4" block="数字4"
 	  NUM4 = 0x14,
-	  //% blockId="NUM5" block="数字5"
 	  NUM5 = 0x15,
-	  //% blockId="NUM6" block="数字6"
 	  NUM6 = 0x16,
-	  //% blockId="NUM7" block="数字7"
 	  NUM7 = 0x18,
-	  //% blockId="NUM8" block="数字8"
 	  NUM8 = 0x19,
-	  //% blockId="NUM9" block="数字9"
 	  NUM9 = 0x1A
 };
 
@@ -87,7 +66,7 @@ namespace Mbit_IR {
   * button pushed.
   */
   //% blockId=ir_received_left_event
-  //% block="当 |%btn| 按钮 按下"
+  //% block="on |%btn| button pressed"
   void onPressEvent(RemoteButton btn, Action body) {
     //if(actions.find(btn) == actions.end()) actions[btn] = new vector();
     actions[btn].push_back(body);
@@ -117,7 +96,7 @@ namespace Mbit_IR {
   * initialises local variablesssss
   */
   //% blockId=ir_init
-  //% block="连接 信号 接收器 到 %pin"
+  //% block="connect ir receiver to %pin"
   void init(Pins pin){
     rx = new ReceiverIR((PinName)pin);
     tsb.start(); //interrupt timer for debounce
