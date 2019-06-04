356284f4f172e867301d6e4f72e5c9abc979b3c1
nan <499983129@qq.com>
2019-06-04 14:49:34 +0800

    enums.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -10,14 +10,14 @@ declare namespace Mbit_IR {
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
+    //% block="当 |%btn| 按钮 按下" shim=Mbit_IR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
+    //% block="连接 信号 接收器 到 %pin" shim=Mbit_IR::init
     function init(pin: Pins): void;
 }
 
